a6f189593b26f0873883899bbff4bb2d288f458a
Mariana Constanza Abalo <abalomariana@gmail.com>
2023-02-20 14:11:29 -0300

    punto 7 - Web funcionando con observaciones + carpeta dist

---
+++ artegalery/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name= 'galeria online',
+    version= '1.0',
+    description= 'galeria de arte online',
+    author= 'Mariana C. Abalo - ladytopo',
+    author_email= 'abalomariana@gmail.com',
+    packages= ['artegalery, agalery']
+)
